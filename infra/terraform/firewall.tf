7956e7c6cb9051b3f44577c4c435821a6d1a31bd
nht <tani1041work@gmail.com>
2026-08-18 13:13:13 +0900

    format修正

--- infra/terraform/firewall.tf
+++ infra/terraform/firewall.tf
@@ -1,25 +1,25 @@
 resource "sakura_packet_filter" "intern-team-c" {
-    name        = "intern-team-c"
-    description = "description"
+  name        = "intern-team-c"
+  description = "description"
 }
 
 resource "sakura_packet_filter_rules" "intern-team-c" {
-    packet_filter_id = sakura_packet_filter.intern-team-c.id
-    expression = [{
-        protocol         = "tcp"
-        destination_port = "22222"
+  packet_filter_id = sakura_packet_filter.intern-team-c.id
+  expression = [{
+    protocol         = "tcp"
+    destination_port = "22222"
     },
     {
-        protocol         = "tcp"
-        destination_port = "80"
+      protocol         = "tcp"
+      destination_port = "80"
     },
     {
-        protocol         = "tcp"
-        destination_port = "443"
+      protocol         = "tcp"
+      destination_port = "443"
     },
     {
-        protocol    = "ip"
-        allow       = false
-        description = "Deny ALL"
-    }]
+      protocol    = "ip"
+      allow       = false
+      description = "Deny ALL"
+  }]
 }
