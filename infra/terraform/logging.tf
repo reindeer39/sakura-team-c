5ea62892e959b1ad07abeef04886715c5a13b94f
nht <tani1041work@gmail.com>
2026-08-20 11:06:58 +0900

    ロギング機能

--- infra/terraform/logging.tf
+++ infra/terraform/logging.tf
@@ -1,17 +1,25 @@
+resource "sakura_monitoring_suite_log_storage" "system" {
+  name = "intern2026-logs-system"
+}
+
 resource "sakura_monitoring_suite_log_storage" "app" {
   name                  = "intern2026-logs"
-  retention_period_days = 30
+  retention_period_days = 40
+}
+
+resource "sakura_monitoring_suite_metric_storage" "system" {
+  name = "intern2026-metrics-system"
 }
 
 resource "sakura_monitoring_suite_metric_storage" "app" {
   name = "intern2026-metrics"
 }
 
-resource "sakura_monitoring_suite_log_routing" "db_slow" {
+resource "sakura_monitoring_suite_log_routing" "db_system" {
   resource_id    = sakura_database.db.id
   storage_id     = sakura_monitoring_suite_log_storage.app.id
   publisher_code = "database"
-  variant        = "slowquerylog"
+  variant        = "systemlog"
 }
 
 resource "sakura_monitoring_suite_metric_routing" "db" {
@@ -27,4 +35,22 @@ resource "sakura_monitoring_suite_log_storage_access_key" "agent" {
 
 resource "sakura_monitoring_suite_metric_storage_access_key" "agent" {
   storage_id = sakura_monitoring_suite_metric_storage.app.id
+}
+
+output "monitoring_logs_endpoint" {
+  value = sakura_monitoring_suite_log_storage.app.endpoints.ingester.address
+}
+
+output "monitoring_logs_token" {
+  value     = sakura_monitoring_suite_log_storage_access_key.agent.secret
+  sensitive = true
+}
+
+output "monitoring_metrics_endpoint" {
+  value = sakura_monitoring_suite_metric_storage.app.endpoints.address
+}
+
+output "monitoring_metrics_token" {
+  value     = sakura_monitoring_suite_metric_storage_access_key.agent.secret
+  sensitive = true
 }
